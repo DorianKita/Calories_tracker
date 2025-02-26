396e167921c3dbb66947c06f59dd9dfc7ec4e98f
Dorian Kita <dorian.kita90@gmail.com>
2025-02-26 22:15:38 +0100

    added authorization to add a record into Sheety api

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ AGE = 35
 
 API_KEY = os.environ.get('API_KEY')
 APP_ID = os.environ.get('APP_ID')
+AUTH_SHEETY = os.environ.get('AUTH_SHEETY')
 EXERCISE_URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
 SHEET_URL = 'https://api.sheety.co/de9862cff422df325716dc388558dc1d/workoutTracking/workouts'
 
@@ -41,6 +42,10 @@ today = datetime.now()
 date = today.strftime('%d/%m/%Y')
 hour = today.strftime('%X')
 
+headers_for_sheety = {
+    'Authorization': AUTH_SHEETY
+}
+
 for exercise in result:
     training_parameters = {
         'workout': {
@@ -51,4 +56,4 @@ for exercise in result:
             'calories': exercise['nf_calories'],
         }
     }
-    requests.post(SHEET_URL,json=training_parameters)
+    requests.post(SHEET_URL,json=training_parameters, headers=headers_for_sheety)
